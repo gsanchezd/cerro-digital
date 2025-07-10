88af95dd4eb4c451f1588fe1fba6137603c03335
Alejandro Sáez <alejandro.sapa@gmail.com>
2025-07-10 10:07:52 -0400

    ADD og image and Footer component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
+  site: "https://cerrodigitaltarapaca.cl",
+  trailingSlash: "always",
+  vite: {
     plugins: [tailwindcss()],
   },
 });
